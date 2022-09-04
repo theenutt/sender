611a31cfe961a1a7b906a0dae68ea0b7859d3e40
theenutt <you@example.com5107624@gmail.com>
2022-09-05 02:27:12 +0300

    done header

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,12 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function(e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
